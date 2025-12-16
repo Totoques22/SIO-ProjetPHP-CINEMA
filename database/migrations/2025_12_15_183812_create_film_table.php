97df98f49a5f77711e82b3b0f78699d866f82b59
Faydhy7 <Mattoirfaydhy@gmail.com>
2025-12-16 10:59:45 +0100

    mise à jour migration

---
+++ database/migrations/2025_12_15_183812_create_film_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('film', function (Blueprint $table) {
+            $table->id('idFil');
+            $table->string('titreFil');
+            $table->string('descFil');
+            $table->string('imgFil');
+            $table->integer ('dureFil');
+            $table->foreignId('idGenre')->constrained('genres', 'idGenre');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('film');
+    }
+};
