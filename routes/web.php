ddb2b0a72d1de50e85d49dbfa7bc60603121f5b1
Croissantdett <eloipoly63@gmail.com>
2025-12-16 11:34:11 +0100

    mise a jour partie header

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/connexion', function () {
+    return view('pages.connexion');
+});
+
+Route::get('/inscription', function () {
+    return view('pages.Inscription');
+});
+
+Route::get('/accueil', function () {
+    return view('pages.header');
+});
+
+Route::get('/accueil-admin', function () {
+    return view('pages.header-admin');
+});
