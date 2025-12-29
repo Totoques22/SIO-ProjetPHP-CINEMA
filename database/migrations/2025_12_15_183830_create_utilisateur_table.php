a7409918c13e5fce8d954d718dacac4494ebef11
Faydhy7 <Mattoirfaydhy@gmail.com>
2025-12-29 11:27:22 +0300

    création de la migration role utilisateur et utilisateur

---
+++ database/migrations/2025_12_15_183830_create_utilisateur_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('utilisateur', function (Blueprint $table) {
+            $table->id('idUti');
+            $table->string('nomUti');
+            $table->string('prenomUti');
+            $table->string('adrUti');
+            $table->string('mdpUti');
+            $table->string('mailUti');
+            $table->string('ageUti');
+            $table->foreignId('idRoleUti')->constrained('role_utilisateur','idRoleUti');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('utilisateur');
+    }
+};
