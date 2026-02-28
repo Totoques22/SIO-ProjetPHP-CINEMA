6f40af165e931e3d04fef8999659332828fc7bfd
Croissantdett <eloipoly63@gmail.com>
2026-02-28 14:55:14 +0100

    Routes

--- routes/web.php
+++ routes/web.php
@@ -4,27 +4,32 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FilmController;
 use App\Http\Controllers\ConnexionController;
 use App\Http\Controllers\InscriptionController;
-use App\Http\Controllers\DeconnexionController;
+use App\Http\Controllers\ActeurController;
+use App\Http\Controllers\RealisateurController;
+use App\Http\Controllers\ScenaristeController;
 
-Route::get('/', function () {
-    return view('pages.accueil');
-})->name('accueil');
+//Route::get('/', function () {
+//    return view('pages.accueil');
+//})->name('accueil');
+//
+//Route::get('/accueil', function () {
+//    return view('pages.accueil');
+//})->name('accueil');
 
-Route::get('/accueil', function () {
-    return view('pages.accueil');
-})->name('accueil');
+//route de test
+/*Route::get('/accueil', function () {
+    return "Connexion effectuée";
+})->name('accueil');*/
 
-Route::get('/connexion', [ConnexionController::class, 'showLogin']);
+Route::get('/connexion', function () {
+    return view('pages.connexion');
+});
 Route::post('/connexion', [ConnexionController::class, 'login'])->name('login');
 
-Route::get('/inscription', [InscriptionController::class, 'showLogin']);
-Route::post('/inscription', [InscriptionController::class, 'sign_in'])->name('sign-in');
-
-Route::post('/deconnexion', [DeconnexionController::class, 'logout'])->name('logout');
-
-Route::get('/accueil-admin', function () {
-    return view('pages.header-admin');
+Route::get('/inscription', function () {
+    return view('pages.Inscription');
 });
+Route::post('/inscription', [InscriptionController::class, 'login'])->name('sign-in');
 
 Route::get('/seance', function () {
     return view('pages.seance');
@@ -34,9 +39,46 @@ Route::get('/connexion_reservation', function () {
     return view('pages.connexion_reservation');
 });
 
+Route::get('/gestion-acteur', function () {
+    return view('pages.gestion-acteur');
+});
+
+Route::get('/gestion-realisateur', function () {
+    return view('pages.gestion-realisateur');
+});
+
+Route::get('/gestion-scenariste', function () {
+    return view('pages.gestion-scenariste');
+});
+
+Route::get('/gestion-cinema', function () {
+    return view('pages.gestion-cinema');
+});
+
+Route::get('/gestion-programmation', function () {
+    return view('pages.gestion-programmation');
+});
+
 Route::get('/inscription_reservation', function () {
     return view('pages.inscription_reservation');
 });
+
+Route::get('/ajout-acteur', function () {
+    return view('pages.ajout-acteur');
+});
+
+Route::get('/ajout-realisateur', function () {
+    return view('pages.ajout-realisateur');
+});
+
+Route::get('/ajout-scenariste', function () {
+    return view('pages.ajout-scenariste');
+});
+
+Route::get('/ajout-cinema', function () {
+    return view('pages.ajout-cinema');
+});
+
 
 Route::get('/tous-les-films', [FilmController::class, 'index'])->name('films.index');
 
@@ -46,7 +88,7 @@ Route::get('/actuellement-au-cinema', [FilmController::class, 'filmsAuCinema'])-
 
 Route::get('/actuellement-au-cinema-admin', [FilmController::class, 'filmsAuCinemaAdmin'])->name('films.admin.cinema');
 
-Route::get('/accueil', [FilmController::class, 'filmsAccueil'])->name('accueil');
+Route::get('/', [FilmController::class, 'filmsAccueil'])->name('accueil');
 
 Route::get('/accueil-admin', [FilmController::class, 'filmsAccueilAdmin'])->name('accueil.admin');
 
@@ -54,4 +96,52 @@ Route::get('/films/{film}', [FilmController::class, 'show'])->name('films.show')
 
 Route::get('/films-admin/{film}', [FilmController::class, 'showAdmin'])->name('films.admin.show');
 
-Route::get('/gestion-films', [FilmController::class, 'GestionAdmin'])->name('films.admin.gestion');
+Route::get('/gestion-films', [FilmController::class, 'index'])->name('films.admin.gestion');
+
+Route::delete('/films/{film}', [FilmController::class, 'destroy'])->name('films.destroy');
+
+Route::get('/ajout-programme', [FilmController::class, 'ajoutProgramme'])->name('ajout.programme');
+
+Route::get('/ajout-film/ajouter', [FilmController::class, 'create'])->name('film.create');
+
+Route::post('/ajout-film/ajouter', [FilmController::class, 'store'])->name('film.store');
+
+Route::get('/admin/films/{id}/edit', [FilmController::class, 'edit'])->name('film.edit');
+
+Route::put('/admin/films/{id}', [FilmController::class, 'update'])->name('film.update');
+
+Route::get('/ajout-acteur/ajouter', [ActeurController::class, 'create'])->name('acteur.create');
+
+Route::get('/gestion-acteur', [ActeurController::class, 'index'])->name('acteur.admin.gestion');
+
+Route::post('/ajout-acteur/ajouter', [ActeurController::class, 'store'])->name('acteur.store');
+
+Route::get('/admin/acteur/{id}/edit', [ActeurController::class, 'edit'])->name('acteur.edit');
+
+Route::put('/admin/acteur/{id}', [ActeurController::class, 'update'])->name('acteur.update');
+
+Route::delete('/acteur/{id}', [ActeurController::class, 'destroy'])->name('acteur.destroy');
+
+Route::get('/gestion-realisateur', [RealisateurController::class, 'index'])->name('realisateur.admin.gestion');
+
+Route::get('/ajout-realisateur/ajouter', [RealisateurController::class, 'create'])->name('realisateur.create');
+
+Route::post('/ajout-realisateur/ajouter', [RealisateurController::class, 'store'])->name('realisateur.store');
+
+Route::get('/admin/realisateur/{id}/edit', [RealisateurController::class, 'edit'])->name('realisateur.edit');
+
+Route::put('/admin/realisateur/{id}', [RealisateurController::class, 'update'])->name('realisateur.update');
+
+Route::delete('/realisateur/{id}', [RealisateurController::class, 'destroy'])->name('realisateur.destroy');
+
+Route::get('/gestion-scenariste', [ScenaristeController::class, 'index'])->name('scenariste.admin.gestion');
+
+Route::get('/ajout-scenariste/ajouter', [ScenaristeController::class, 'create'])->name('scenariste.create');
+
+Route::post('/ajout-scenariste/ajouter', [ScenaristeController::class, 'store'])->name('scenariste.store');
+
+Route::get('/admin/scenariste/{id}/edit', [ScenaristeController::class, 'edit'])->name('scenariste.edit');
+
+Route::put('/admin/scenariste/{id}', [ScenaristeController::class, 'update'])->name('scenariste.update');
+
+Route::delete('/scenariste/{id}', [ScenaristeController::class, 'destroy'])->name('scenariste.destroy');
