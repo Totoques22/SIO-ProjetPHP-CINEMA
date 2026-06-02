8fc4435e189aa0a4b6401d704c5f3bce0c0c4bad
Totoques22 <thomas.quesada.pro@gmail.com>
2026-06-02 12:32:33 +0200

    bugfix nom en majuscule des tables dans les modele alors quil ne devrait pas

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model{
-    protected $table ='Reservation';
+    protected $table ='reservation';
     protected $primaryKey = 'idRes';
     public $timestamps = false ;
     protected $fillable=[
